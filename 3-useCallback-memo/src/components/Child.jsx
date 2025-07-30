7788e934739038f15ab3f3ad3cf47b19d4ecdd5a
Arav3103 <arav3103@gmail.com>
2025-07-30 17:48:55 +0530

    Parent>Child>GrandChild example

--- 3-useCallback-memo/src/components/Child.jsx
+++ 3-useCallback-memo/src/components/Child.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useRef } from "react";
+import Grandchild from "./Grandchild";
 
-const Child = () => {
-  return <div>Child</div>;
-};
+const Child = React.memo(function ({ handleClick }) {
+  const count = useRef(0);
+  count.current += 1;
+  return (
+    <>
+      <button onClick={handleClick}>Click me!!</button>
+      <p>Child component rendered {count.current} times</p>
+      <Grandchild input={count.current} />
+    </>
+  );
+});
 
 export default Child;
--- 3-useCallback-memo/src/components/Parent.jsx
+++ 3-useCallback-memo/src/components/Parent.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useCallback, useState } from "react";
+import Child from "./Child";
 
 const Parent = () => {
-  return (
-    <div>Parent</div>
-  )
-}
+  const [flag, setFlag] = useState(false);
+  const [count, setCount] = useState(0);
+  const handleClick = useCallback(() => {
+    setFlag((prevValue) => !prevValue);
+  }, []);
+  const updateCount = () => {
+    setCount((prev) => prev + 1);
+  };
 
-export default Parent
+  return (
+    <>
+      <p>Value is {flag ? "True" : "False"}</p>
+      <button onClick={updateCount}>Update Count</button>
+      <p>Count is {count}</p>
+      <Child handleClick={handleClick} />
+    </>
+  );
+};
+
+export default Parent;
